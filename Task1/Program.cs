ac9072d23325daca498b52196c63c7e3fab9d693
Maria Garmash <garmash.masha.et-301@mail.ru>
2024-02-15 20:42:06 +0300

    пredoПП

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//Напишите программу, которая на вход принимает два целых числа
+// и проверяет, является ли первое число квадратом второго.
+
+// Console.Write("Введите первое число: ");
+// int number1 = Console.ReadLine();
+// // int.Parse("cczczczxc");
+// // Convert.ToInt32("1125546");
+// Console.Write("Введите второе число: ");
+// int number2 = Console.ReadLine();
+
+// if (number1 == number2 * number2)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
