52e5155d7e7c62da87804c6c316700fc5bc8e73e
Maria Garmash <garmash.masha.et-301@mail.ru>
2024-02-15 21:44:26 +0300

    another way to make Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -31,9 +31,16 @@
 // int num3 = num / 10 % 10;
 // Console.WriteLine(num3);
 
-int num = new Random().Next(100, 1000);
+// int num = new Random().Next(100, 1000);
 
-int num1 = num % 10;
-int num2 = num / 100;
+// int num1 = num % 10;
+// int num2 = num / 100;
 
-Console.WriteLine($"Число {num}. Сумма {num1} и {num2} равна {num1 + num2}");
+// Console.WriteLine($"Число {num}. Сумма {num1} и {num2} равна {num1 + num2}");
+
+
+
+Console.Write("Введите трехзначное число: ");
+string num = Console.ReadLine()!;
+Console.WriteLine(Convert.ToInt32(Convert.ToString(num[0])) +
+                    Convert.ToInt32(Convert.ToString(num[2])));
